6247c0f463602684525b3016e18382ded9208d1e
Nagaraj Lakshatti <nagaraj.lakshatti11@gmail.com>
2026-01-23 01:51:44 +0530

    okok

--- radish/environment.py
+++ radish/environment.py
@@ -14,28 +14,45 @@ from utils.driver_manager import get_driver
 
 
 
+# @before.each_scenario
+# def start_browser(scenario):
+#     scenario.context.driver = get_driver()
+#
+#
+# @after.each_scenario
+# def close_browser(scenario):
+#     driver = scenario.context.driver
+#
+#     if scenario.state == "FAILED":
+#         # 📸 Screenshot
+#         allure.attach(
+#             driver.get_screenshot_as_png(),
+#             name="Failure Screenshot",
+#             attachment_type=allure.attachment_type.PNG
+#         )
+#
+#         # 📄 Page Source
+#         allure.attach(
+#             driver.page_source,
+#             name="Page Source",
+#             attachment_type=allure.attachment_type.HTML
+#         )
+#
+#     driver.quit()
+
 @before.each_scenario
 def start_browser(scenario):
     scenario.context.driver = get_driver()
 
-
 @after.each_scenario
-def close_browser(scenario):
-    driver = scenario.context.driver
+def stop_browser(scenario):
+    if hasattr(scenario.context, "driver"):
+        scenario.context.driver.quit()
 
-    if scenario.state == "FAILED":
-        # 📸 Screenshot
-        allure.attach(
-            driver.get_screenshot_as_png(),
-            name="Failure Screenshot",
-            attachment_type=allure.attachment_type.PNG
-        )
-
-        # 📄 Page Source
-        allure.attach(
-            driver.page_source,
-            name="Page Source",
-            attachment_type=allure.attachment_type.HTML
-        )
-
-    driver.quit()
+@after.each_step
+def screenshot_on_failure(step):
+    driver = getattr(step.context, "driver", None)
+    if driver and step.state == "failed":
+        os.makedirs("allure-results/screenshots", exist_ok=True)
+        path = f"allure-results/screenshots/{step.id}.png"
+        driver.save_screenshot(path)
